b67768f3364fdb38f834eddeea14a17d9c6f19da
Florian Juengermann <florianjuengermann@gmail.com>
2023-05-11 18:42:23 +0000

    added hyde

---
+++ llm.py
@@ -0,0 +1,56 @@
+import openai
+import dotenv
+import os
+import json
+
+dotenv.load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+CACHE_DIR = "cache"
+os.makedirs(CACHE_DIR, exist_ok=True)
+CACHE_FILE = f"{CACHE_DIR}/llm_cache.json"
+
+if os.path.exists(CACHE_FILE):
+    with open(CACHE_FILE) as f:
+        cache = json.load(f)
+else:
+    cache = {}
+
+def complete(sys_promt, user_input, model="gpt-4", temp=0.0, max_tokens=None, n=1, return_list=False):
+    if model=="turbo":
+        model = "gpt-3.5-turbo"
+    messages = [
+        {"role": "system", "content": sys_promt},
+        {"role": "user", "content": user_input},
+    ]
+    cache_key = json.dumps([model, messages, n])
+    if cache_key in cache:
+        print("using cached result")
+        return cache[cache_key]
+    print("running completion")
+    
+    r = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temp,
+        max_tokens=max_tokens,
+        n=n,
+    )
+    if n == 1 and not return_list:
+        res = r.choices[0]["message"]["content"]
+    else:
+        res = [choice["message"]["content"] for choice in r.choices]
+
+    cache[cache_key] = res
+    with open(CACHE_FILE, "w") as f:
+        json.dump(cache, f)
+    return res
+
+def complete_json(sys_promt, user_input, model="gpt-4"):
+    res = complete(sys_promt, user_input, model=model)
+    try:
+        return json.loads(res)
+    except json.decoder.JSONDecodeError:
+        print("Error decoding JSON:")
+        print(res)
+        return None
